39450562c46df7566455d5b59c468e573b2e8e18
shohan494 <shohan12224943@gmail.com>
2017-11-05 09:39:33 +0600

    controller and methods, runs as url

---
+++ application/controllers/Shop.php
@@ -0,0 +1,12 @@
+<?php
+class Shop extends CI_Controller {
+
+		    public function index()
+		    {
+		            echo 'WELCOME TO THE SHOP';
+		    }
+        public function comments()
+        {
+                echo 'Look at this!';
+        }
+}
